56c8947f5236274e9d8945dde87eb04bb6abe2ef
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-15 10:47:23 -0400

    Removing unnecessary global variables

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const API_KEY = "ee8c332cbd72a23de5f8e2a32d0e2337";
 
-let latitude;
-let longtitude;
-
-const fetchWeatherData = (units = "imperial", lang = "EN") => {
+const fetchWeatherData = (
+  latitude,
+  longtitude,
+  units = "imperial",
+  language = "en"
+) => {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&units=${units}&lang=${language}&lon=${longtitude}&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => console.log(data));
@@ -17,9 +19,9 @@ const fetchGeolocationData = (zipCode, countryCode = "US") => {
   )
     .then((response) => response.json())
     .then((data) => {
-      latitude = data.lat;
-      longtitude = data.lon;
-      fetchWeatherData();
+      const latitude = data.lat;
+      const longtitude = data.lon;
+      fetchWeatherData(latitude, longtitude);
       console.log(data, latitude, longtitude);
     });
 };
