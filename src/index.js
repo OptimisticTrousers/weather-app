d1e1306e96cfbdfeae926f37a568a423e8ac81ba
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2022-06-15 15:21:54 -0400

    Fixing typos in variables and adding a new function that fetches the weather forecast

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ const API_KEY = "ee8c332cbd72a23de5f8e2a32d0e2337";
 
 const fetchWeatherData = (
   latitude,
-  longtitude,
+  longitude,
   units = "imperial",
   language = "en"
 ) => {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&units=${units}&lang=${language}&lon=${longtitude}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&units=${units}&lang=${language}&lon=${longitude}&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => console.log(data));
 };
 
-export default fetchGeolocationData = (zipCode, countryCode = "US") => {
+function fetchForecast(latitude, longitude) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+const fetchGeolocationData = (zipCode, countryCode = "US") => {
   fetch(
     `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
       const latitude = data.lat;
-      const longtitude = data.lon;
-      fetchWeatherData(latitude, longtitude);
-      console.log(data, latitude, longtitude);
+      const longitude = data.lon;
+      fetchWeatherData(latitude, longitude);
+      fetchForecast(latitude, longitude);
     });
 };
+export default fetchGeolocationData;
 
 fetchGeolocationData("07087");
