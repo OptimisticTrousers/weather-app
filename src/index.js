de25279a2d603ed2636eeb009fdc7313a39a93eb
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-15 08:14:36 -0400

    Configuring Prettier and ESLint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const API_KEY = 'ee8c332cbd72a23de5f8e2a32d0e2337';
+const API_KEY = "ee8c332cbd72a23de5f8e2a32d0e2337";
 
 function fetchGeolocationData(city, state, limit = 5) {
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name},${state_code},{country code}&limit=${limit}&appid=${API_KEY}`);
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city_name},${state_code},{country code}&limit=${limit}&appid=${API_KEY}`
+  );
 }
-console.log('bob');
+console.log("bob");
