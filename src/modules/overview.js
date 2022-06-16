3e93ba89629ffac235543432846df0407269e3a2
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-16 16:46:43 -0400

    Fixing stuff

--- src/modules/overview.js
+++ src/modules/overview.js
@@ -3,9 +3,15 @@ export default function renderOverviewData(weatherData) {
   const city = document.querySelector("p.city");
   const time = document.querySelector("p.time");
   const weatherType = document.querySelector(".weather-type > p");
+  const weatherIcon = document.querySelector(".weather-type > img");
+
+  const condition = weatherData.weather[0].main;
 
   degrees.textContent = `${Math.round(weatherData.main.temp)}°`;
   city.textContent = `${weatherData.name}, ${weatherData.sys.country}`;
   time.textContent = weatherData.dt;
-  weatherType.textContent = weatherData.weather[0].main;
+  weatherType.textContent = condition;
+}
+
+function renderIcon(condition){
 }
