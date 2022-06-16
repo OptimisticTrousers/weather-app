01c81c35bd71da48545b8228a12845864e773719
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-16 16:06:33 -0400

    Fixing bug where 'History' could be larger than 4 items

--- src/modules/history.js
+++ src/modules/history.js
@@ -2,17 +2,26 @@ import renderOverviewData from "./overview";
 import renderWeatherDetails from "./weatherDetails";
 
 export default function addCityToHistory(weatherData) {
+
   const history = document.querySelector("div.cities");
     const cityName = document.createElement("p");
     cityName.textContent = weatherData.name;
 
-    history.appendChild(cityName);
-    history.removeChild(history.firstChild);
+    if(history.childElementCount >= 3){
+
+      history.appendChild(cityName);
+      history.removeChild(history.firstChild);
+    }
+    else{
+      history.appendChild(cityName);
+
+    }
 
     cityName.addEventListener("click", () => {
       renderWeatherDetails(weatherData);
       renderOverviewData(weatherData);
     });
+
 
 
 
