810e2965880811f371640e134782f20281053c85
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-20 15:24:05 -0400

    This

--- src/modules/image.js
+++ src/modules/image.js
@@ -3,16 +3,16 @@ export default function renderImage(condition) {
   const weatherCondition = condition.toLowerCase();
 
   if (weatherCondition.includes("thunder")) {
-    main.style.backgroundImage = "url('./images/thunder.jpg')";
+    main.style.backgroundImage = "url('images/thunder.jpg')";
   } else if (weatherCondition.includes("rain")) {
-    main.style.backgroundImage = "url('./images/rain.jpg')";
+    main.style.backgroundImage = "url('images/rain.jpg')";
   } else if (weatherCondition.includes("snow")) {
-    main.style.backgroundImage = "url('./images/snow.jpg')";
+    main.style.backgroundImage = "url('images/snow.jpg')";
   } else if (weatherCondition.includes("clear")) {
-    main.style.backgroundImage = "url('./images/clear.jpg')";
+    main.style.backgroundImage = "url('images/clear.jpg')";
   } else if (weatherCondition.includes("cloud")) {
-    main.style.backgroundImage = "url('./images/cloud.jpg')";
+    main.style.backgroundImage = "url('images/cloud.jpg')";
   } else {
-    main.style.backgroundImage = "url(./images/fog.jpg)";
+    main.style.backgroundImage = "url('images/fog.jpg')";
   }
 }
