0edc19b4d063570c8866613214ba4de0192f0a95
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-22 16:39:01 -0400

    Changing location of image.js

---
+++ image.js
@@ -0,0 +1,18 @@
+export default function renderImage(condition) {
+  const main = document.querySelector("main");
+  const weatherCondition = condition.toLowerCase();
+
+  if (weatherCondition.includes("thunder")) {
+    main.style.backgroundImage = "url('images/thunder.jpg')";
+  } else if (weatherCondition.includes("rain")) {
+    main.style.backgroundImage = "url('images/rain.jpg')";
+  } else if (weatherCondition.includes("snow")) {
+    main.style.backgroundImage = "url('images/snow.jpg')";
+  } else if (weatherCondition.includes("clear")) {
+    main.style.backgroundImage = "url('images/clear.jpg')";
+  } else if (weatherCondition.includes("cloud")) {
+    main.style.backgroundImage = "url('images/cloud.jpg')";
+  } else {
+    main.style.backgroundImage = "url('images/fog.jpg')";
+  }
+}
